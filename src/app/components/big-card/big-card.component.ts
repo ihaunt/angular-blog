7cadd0f78ca2d8d25909ec4c77d886be1ccbae91
Jennifer Machado <dev.jenni@outlook.com>
2024-02-22 17:42:15 +0000

    bigcard

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  photoCover:string = 'https://secureservercdn.net/166.62.110.60/h65.3a1.myftpupload.com/wp-content/uploads/2021/09/variable-placeholder-product-31.jpg?time=1644500349'
+  cardTitle:string ='PAO DE QUEIJO'
+  cardDescription:string='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim illo asperiores vero, dicta, est tempore voluptatum laboriosam corporis excepturi, quas culpa incidunt iure ipsa expedita obcaecati aperiam earum eligendi consequatur.'
+
   constructor() { }
 
   ngOnInit(): void {
